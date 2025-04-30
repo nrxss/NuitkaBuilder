b5bba74727062ae4569474eae978c927209c0f46
Nikos Roussis <52631245+neek8044@users.noreply.github.com>
2025-04-30 16:57:55 +0300

    print statement styling

--- main.py
+++ main.py
@@ -12,22 +12,24 @@ try:
     from colorama import Fore, Style
 except ImportError:
     while True:
-        answer = input("--> Module 'colorama' not found. Run 'pip3 install colorama' now? [y/n] : ").lower()
+        answer = input("▷ - Module 'colorama' not found. Run 'pip3 install colorama' now? [y/n] : ").lower()
         if answer == "n":
             print("Aborted.")
             sys.exit()
         elif answer == 'y':
             for cmd in ["pip", "pip3", "python -m pip", "python3 -m pip"]:
-                print(f"◼ Attempting intallation using {cmd}...")
+                print(f"◻ - Attempting intallation using {cmd}...")
                 if os.system(f"{cmd} install --upgrade colorama") == 0:
                     break
             else:
-                print("✖ Failed to install colorama. Please install it manually.")
+                print("✖ - Failed to install colorama. Please install it manually.")
                 sys.exit(1)
-            print("✔ Module installation complete. Please re-run.")
+            print("◼ - Module installation finished. Please re-run.")
             sys.exit(0)
         else:
-            print("Invalid input. Answer with 'y' (yes) or 'n' (no).")
+            print("⚠ - Invalid input. Answer with 'y' (yes) or 'n' (no).")
+
+print("✔ - Initialization successful.")
 
 # Parsing options with argv
 extras = []
@@ -55,7 +57,7 @@ print(
     Style.NORMAL, Fore.YELLOW, 
     "\n\nNOTE: Linux requires 'patchelf' to be installed. You should install it with your package manager to use this program.\nYou may want to install 'ordered-set' with 'pip3 install ordered-set' for best compilation performance.",  
     Fore.BLUE, 
-    "\n--> Building standalone for main.py", 
+    "\n◻ Building standalone for main.py", 
     Fore.RESET
 )
 
@@ -63,7 +65,7 @@ print(
 start = time.time()
 full_command = str("wine " if wine else "") + f"python{'' if wine else '3'} -m nuitka --standalone {extras} --follow-imports --output-dir=\"{cwd}/output/\" main.py"
 
-print(Fore.MAGENTA + "Executing: " + full_command + Fore.RESET) if debug else ...
+print(Fore.MAGENTA + "◻ Executing: " + full_command + Fore.RESET) if debug else ...
 
 nuitka = subprocess.Popen(
     full_command, 
@@ -77,7 +79,7 @@ anim_chars = ["\\", "|", "/", "-"]
 
 while nuitka.poll() is None:
     for i in anim_chars:
-        print("--> Please wait, compiling... " + i, " "*10 if wine else "", end="\r")
+        print("◻ Please wait, compiling... " + i, " "*10 if wine else "", end="\r")
         time.sleep(0.1)
 
 end = time.time()
@@ -85,10 +87,10 @@ timeout = 3
 
 # Code cannot be compiled as quickly as the timeout set, thus it must have failed. (might improve in the future)
 if end - start < timeout:
-    print(Fore.RED + "ERROR: Unknown issue with building main.py.\nTroubleshooting:\n\t-> Make sure to have a 'main.py' file in this local folder.\n\t-> Make sure to have Nuitka installed in your Python3 installation.\n\t-> If you are using the Wine compatibility layer (Linux only), check if Wine is installed and that you have Python 3 with Nuitka installed inside of your default Wine config." + Fore.RESET)
+    print(Fore.RED + "✖ - Unknown issue with building main.py.\nTroubleshooting:\n\t-> Make sure to have a 'main.py' file in this local folder.\n\t-> Make sure to have Nuitka installed in your Python3 installation.\n\t-> If you are using the Wine compatibility layer (Linux only), check if Wine is installed and that you have Python 3 with Nuitka installed inside of your default Wine config." + Fore.RESET)
 else:
-    print(Fore.GREEN + "\nFinished building main.py (stored in: ./output/main.dist/)" + Fore.RESET)
+    print(Fore.GREEN + "\n◼ Finished building main.py (stored in: ./output/main.dist/)" + Fore.RESET)
 
-input("\nPress ENTER to exit...")
+input("\n▷ - Press ENTER to exit...")
 sys.exit()
 
